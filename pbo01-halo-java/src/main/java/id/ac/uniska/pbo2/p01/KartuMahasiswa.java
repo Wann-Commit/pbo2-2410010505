81b360baf5d510618974691ad2608bd09bc1b2e6
Wann-Commit <rakaa1820@gmail.com>
2026-09-22 16:44:33 +0800

    P01 : Membuat kartu Mahasiswa menggunakan Java

---
+++ pbo01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author USER
+ */
+public class KartuMahasiswa {
+    
+    public static void main(String[] args) {
+        String nama = "Muhammad Raka Wisudawan Abhar";
+        String npm = "2410010505";
+        String prodi = "Teknik Informatika";
+        int sem = 5;
+        String als = "Karna ingin memperdalam pembelajaran PBO";
+        
+        System.out.println("============================================================");
+        System.out.println("KARTU MAHASISWA PBO 2");
+        System.out.println("============================================================");
+        System.out.println("NAMA        : " + nama);
+        System.out.println("NPM         : " + npm);
+        System.out.println("PRODI       : " + prodi);
+        System.out.println("SEMESTER    : " + sem);
+        System.out.println("ALASAN      : " + als);
+        System.out.println("============================================================");
+             
+        
+    }
+    
+}
